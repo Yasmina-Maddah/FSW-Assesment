e661aae4d83f5ce1558d0f800f707cc4cdc91ce8
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-02 11:46:34 +0200

    Added the Pivot Table's Columns

--- laravel/database/migrations/2024_12_02_094209_create_project_user_table.php
+++ laravel/database/migrations/2024_12_02_094209_create_project_user_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('project_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('project_id')->constrained('projects')->onDelete('cascade');
             $table->timestamps();
         });
     }
