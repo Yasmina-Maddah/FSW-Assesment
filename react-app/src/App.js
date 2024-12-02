8e5cc241d646469ebeb4910e6162fdf4c67f5ac7
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-02 11:24:36 +0200

    Included Navigate, and added a Catch all Route to handle Undefined Paths

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import Projects from "./pages/Projects";
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/projects" />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
       </BrowserRouter>
