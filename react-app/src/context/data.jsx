1d98094fc92907aee6b4bfa3c16be1085cc7acc2
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-02 11:32:49 +0200

    Added the projects data in the context folder

---
+++ react-app/src/context/data.jsx
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+      id: 1,
+      name: "Project One",
+      description: "This is the first project.",
+      members: [
+        { id: 1, name: "Jana", color: "red" },
+        { id: 2, name: "Ghadi", color: "blue" },
+      ],
+    },
+    {
+      id: 2,
+      name: "Project Two",
+      description: "This is the second project.",
+      members: [
+        { id: 3, name: "Noura", color: "green" },
+        { id: 4, name: "Samer", color: "yellow" },
+      ],
+    },
+  ];
